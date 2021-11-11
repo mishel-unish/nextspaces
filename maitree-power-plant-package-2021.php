68cd8b5ee3931421bec67fb3b5e70057c0751f26
Mishel <mishel.unish@gmail.com>
2021-11-11 15:11:19 +0600

    Complete maitree power plant rmc package 2021 project page

--- maitree-power-plant-package-2021.php
+++ maitree-power-plant-package-2021.php
@@ -20,9 +20,11 @@
 			<!-- Head Section -->
 			<section class="small-section rnpp-bg mt-70">
 				<div class="relative container align-left">
-					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Rooppur Nuclear Power Plant</h1>
+					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Maitree Power Plant RMC</h1>
 					<div class="mod-breadcrumbs font-alt">
-						<a href="index.php">Home</a> / <a href="ground-engineering.php">Ground Engineering</a> / <span>Rooppur Nuclear Power Plant</span>
+						<a href="index.php">Home</a> /
+						<a href="ready-mix-concrete.php">Ready-Mix Concrete</a> /
+						<span>Maitree Power Plant RMC</span>
 					</div>
 				</div>
 			</section>
@@ -64,35 +66,35 @@
 										<tr>
 											<th><i class="fa fa-file-text" aria-hidden="true"></i></th>
 											<th>Project Name</th>
-											<td>Rooppur Nuclear Power Plant Project</td>
+											<td>Maitree Super Thermal Power Plant</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-map-marker" aria-hidden="true"></i></th>
 											<th>Location</th>
-											<td>Rooppur, Ishwardi, Pabna</td>
+											<td>Rampal, Bagherhaat, Khulna, Bangladesh</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-user" aria-hidden="true"></i></th>
 											<th>Contractor</th>
 											<td>
-												<img src="dist/images/contractor/Atomstroyexport-logo.png"
-													alt="Atomstroyexport logo">
-												Atomstroyexport, Joint Stock Company, Russian Federation
+												<img src="dist/images/contractor/bharat-heavy-electricals-limited.png"
+													alt="Bharat Heavy Electricals Limited logo">
+												Bharat Heavy Electricals Limited
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-files-o" aria-hidden="true"></i></th>
 											<th>Scope of Work</th>
 											<td>
-												Priority construction and installation work of preparatory
-												period at the site of Rooppur Nuclear Power Plant and supply
-												equipment including performance of construction and erection works
+												Providing ready mix concrete for 2X660 MW maitree super
+												thermal power project at moidara village, Rampal Upazilla,
+												Bagherhaat District, Bangladesh.
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
 											<th>Duration</th>
-											<td>October, 2016 - June, 2019</td>
+											<td>July 2021 - Ongoing</td>
 										</tr>
 									</tbody>
 								</table>
@@ -100,61 +102,50 @@
 						</div>
 
 						<div class="tab-pane fade" id="map">
-							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3642.8910459878716!2d89.04506121464098!3d24.070141084432713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fea10d4f28cc1d%3A0x117abf1575809234!2sRooppur%20Nuclear%20Power%20Plant!5e0!3m2!1sen!2sbd!4v1636509273914!5m2!1sen!2sbd"
+						<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13032.47298623047!2d89.56082298378553!3d22.59349041098829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0027ded876dfd5%3A0x5e99f547b4dbb2f9!2sBangladesh%20India%20Friendship%20Power%20company%20(Private)%20Limited!5e0!3m2!1sen!2sbd!4v1636609792503!5m2!1sen!2sbd"
 								allowfullscreen="" loading="lazy"></iframe>
 						</div>
 
 						<div class="tab-pane fade" id="gallery">
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale.jpg"
+							<a href="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/material-storage-for-batching-plant.jpg"
 								data-sub-html="#c1">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale-thumb.jpg"
-									alt="Cement storage facility at site for uninterrupted supply with weighing scale"/>
+									src="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/thumb/material-storage-for-batching-plant-450w.jpg"
+									alt="Material storage for batching plant"/>
 								<p class="caption" id="c1"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deep-cement-soil-mixing-with-hyhdraulic-rig-machine.jpg"
+							<a href="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/organized-batching-plant-area.jpg"
 								data-sub-html="#c2">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deep-cement-soil-mixing-with-hyhdraulic-rig-machine-thumb.jpg"
-									alt="Deep cement soil mixing with hyhdraulic rig machine"/>
+									src="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/thumb/organized-batching-plant-area-450w.jpg"
+									alt="Organized batching plant area"/>
 								<p class="caption" id="c2"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deployment-of-multiple-hydraulic-rig-machines.jpg"
+							<a href="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/organized-plant-area.jpg"
 								data-sub-html="#c3">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deployment-of-multiple-hydraulic-rig-machines-thumb.jpg"
-									alt="Deployment of multiple hydraulic rig machines"/>
+									src="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/thumb/organized-plant-area-450w.jpg"
+									alt="Organized plant area"/>
 								<p class="caption" id="c3"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/hydraulic-rotary-rig-and-excavators-in-operation.jpg"
+							<a href="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/silo-for-concrete-storage-and-disbursement-.jpg"
 								data-sub-html="#c4">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/hydraulic-rotary-rig-and-excavators-in-operation-thumb.jpg"
-									alt="Hydraulic rotary rig and excavators in operation"/>
+									src="dist/images/projects/ready-mix-concrete/maitree-power-plant-package-2021/thumb/silo-for-concrete-storage-and-disbursement--450w.jpg"
+									alt="Silo for concrete storage and disbursement"/>
 								<p class="caption" id="c4"></p>
-								<div class="view">
-									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
-									View Gallery
-								</div>
-							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/simultaneously-working-hydraulic-rig-machines-at-site.jpg"
-								data-sub-html="#c5">
-								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/simultaneously-working-hydraulic-rig-machines-at-site-thumb.jpg"
-									alt="Simultaneously working hydraulic rig machines at site"/>
-								<p class="caption" id="c5"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
