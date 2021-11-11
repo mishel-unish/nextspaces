34aff71608f9f7bb7823f29fd8fb3e51ff6d7e69
Mishel <mishel.unish@gmail.com>
2021-11-11 15:23:50 +0600

    Complete ashugonj 450mw power plant project page

--- ashuganj-450mw-ccpp-south.php
+++ ashuganj-450mw-ccpp-south.php
@@ -18,13 +18,13 @@
 			<?php include './shared/header.html'; ?>
 
 			<!-- Head Section -->
-			<section class="small-section rnpp-bg mt-70">
+			<section class="small-section accpp-bg mt-70">
 				<div class="relative container align-left">
-					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">RooppurNuclearPowerPlant</h1>
+					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Ashuganj 450MW Power Plant</h1>
 					<div class="mod-breadcrumbs font-alt">
 						<a href="index.php">Home</a> /
 						<a href="ready-mix-concrete.php">Ready-Mix Concrete</a> /
-						<span>RooppurNuclearPowerPlant</span>
+						<span>Ashuganj 450MW Power Plant</span>
 					</div>
 				</div>
 			</section>
@@ -66,33 +66,35 @@
 										<tr>
 											<th><i class="fa fa-file-text" aria-hidden="true"></i></th>
 											<th>Project Name</th>
-											<td>RooppurNuclear</td>
+											<td>Ashuganj 450MW Combined Cycle Power Plant</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-map-marker" aria-hidden="true"></i></th>
 											<th>Location</th>
-											<td>RooppurIshwardiPabna</td>
+											<td>Ashuganj, Brahmanbaria, Bangladesh.</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-user" aria-hidden="true"></i></th>
 											<th>Contractor</th>
 											<td>
-												<img src="dist/images/contractor"
-													alt="Atomstroyexport logo">
-												Atomstroyexport
+												<!-- <img src="dist/images/contractor"
+													alt="TSK Electronica y Electricidad e Inelectra Internation AB logo"> -->
+												TSK Electronica y Electricidad e Inelectra Internation AB
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-files-o" aria-hidden="true"></i></th>
 											<th>Scope of Work</th>
 											<td>
-												Priorityconstruction
+												Design, supply, installation and start-up of 3 batching plants
+												with unitary nominal capacity of 50 m3/h in turn-key basis for
+												Ashuganj 450MW CCPP South,Bangladesh.
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
 											<th>Duration</th>
-											<td>October2016 - June2019</td>
+											<td>August, 2013 - February, 2014</td>
 										</tr>
 									</tbody>
 								</table>
@@ -100,17 +102,83 @@
 						</div>
 
 						<div class="tab-pane fade" id="map">
-							<iframe class="map" src="https"
+							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3643.609433361324!2d91.01458163078725!3d24.044834267855908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37541b2d8c6afb0d%3A0x72435629268be777!2sAshuganj%20450MW%20Combined%20Cycle%20Power%20Plant%20North!5e0!3m2!1sen!2sbd!4v1636622193691!5m2!1sen!2sbd"
 								allowfullscreen="" loading="lazy"></iframe>
 						</div>
 
 						<div class="tab-pane fade" id="gallery">
-							<a href="dist/images/projects/ready-mix-concrete"
-								data-sub-html="#c">
+							<a href="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/casting-with-long-boom.jpg"
+								data-sub-html="#c1">
 								<img
-									src="dist/images/projects/ready-mix-concrete"
-									alt="Cement"/>
-								<p class="caption" id="c"></p>
+									src="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/thumb/casting-with-long-boom-450w.jpg"
+									alt="Casting with long boom"/>
+								<p class="caption" id="c1"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/large-scale-casting-with-long-boom-pumps.jpg"
+								data-sub-html="#c2">
+								<img
+									src="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/thumb/large-scale-casting-with-long-boom-pumps-450w.jpg"
+									alt="Large-scale casting with long boom pumps"/>
+								<p class="caption" id="c2"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/maintained-batching-plant-with-proper-machineries.jpg"
+								data-sub-html="#c3">
+								<img
+									src="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/thumb/maintained-batching-plant-with-proper-machineries-450w.jpg"
+									alt="Maintained batching plant with proper machineries"/>
+								<p class="caption" id="c3"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/material-utilized-with-modern-equipments.jpg"
+								data-sub-html="#c4">
+								<img
+									src="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/thumb/material-utilized-with-modern-equipments-450w.jpg"
+									alt="Material utilized with modern equipments"/>
+								<p class="caption" id="c4"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/nde-and-snamm-removed.jpg"
+								data-sub-html="#c5">
+								<img
+									src="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/thumb/nde-and-snamm-removed-450w.jpg"
+									alt="NDE and SNAMM Removed"/>
+								<p class="caption" id="c5"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/organized-storage-of-plant-materials.jpg"
+								data-sub-html="#c6">
+								<img
+									src="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/thumb/organized-storage-of-plant-materials-450w.jpg"
+									alt="Organized storage of plant materials"/>
+								<p class="caption" id="c6"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/transit-mixer-refilling-concrete-from-plant.jpg"
+								data-sub-html="#c7">
+								<img
+									src="dist/images/projects/ready-mix-concrete/ashuganj-450mw-ccpp-south/thumb/transit-mixer-refilling-concrete-from-plant-450w.jpg"
+									alt="Transit mixer refilling concrete from plant"/>
+								<p class="caption" id="c7"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
