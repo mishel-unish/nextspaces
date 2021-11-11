7f3fced55b9aa4d02b1bef55bf63a91dcbb980b6
Mishel <mishel.unish@gmail.com>
2021-11-11 10:41:39 +0600

    Complete color city project page

--- color-city-project.php
+++ color-city-project.php
@@ -18,11 +18,13 @@
 			<?php include './shared/header.html'; ?>
 
 			<!-- Head Section -->
-			<section class="small-section rnpp-bg mt-70">
+			<section class="small-section color-city-bg mt-70">
 				<div class="relative container align-left">
-					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Rooppur Nuclear Power Plant</h1>
+					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Color City Piling Project</h1>
 					<div class="mod-breadcrumbs font-alt">
-						<a href="index.php">Home</a> / <a href="ground-engineering.php">Ground Engineering</a> / <span>Rooppur Nuclear Power Plant</span>
+						<a href="index.php">Home</a> /
+						<a href="ground-engineering.php">Ground Engineering</a> /
+						<span>Color City Piling Project</span>
 					</div>
 				</div>
 			</section>
@@ -64,35 +66,39 @@
 										<tr>
 											<th><i class="fa fa-file-text" aria-hidden="true"></i></th>
 											<th>Project Name</th>
-											<td>Rooppur Nuclear Power Plant Project</td>
+											<td>Color City Piling Project</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-map-marker" aria-hidden="true"></i></th>
 											<th>Location</th>
-											<td>Rooppur, Ishwardi, Pabna</td>
+											<td>Nayapara, Kashimpur, Gazipur</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-user" aria-hidden="true"></i></th>
 											<th>Contractor</th>
 											<td>
-												<img src="dist/images/contractor/Atomstroyexport-logo.png"
-													alt="Atomstroyexport logo">
-												Atomstroyexport, Joint Stock Company, Russian Federation
+												<!-- <img src="dist/images/contractor"
+													alt="Color City Limited logo"> -->
+												Color City Limited
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-files-o" aria-hidden="true"></i></th>
 											<th>Scope of Work</th>
 											<td>
-												Priority construction and installation work of preparatory
-												period at the site of Rooppur Nuclear Power Plant and supply
-												equipment including performance of construction and erection works
+												Construction of Cast-in-situ RCC pile by Hydraulic Rotary
+												Rig. Boring including concreting by RMC (supplied by Color
+												City Limited), rebar (supplied by Color City Limited),
+												Fabrication with welding (including of all materials
+												and tools and equipment’s) and soil removing to a safe
+												distance (within 01 Km) by dump truck and Pile integrity
+												test for all piles
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
 											<th>Duration</th>
-											<td>October, 2016 - June, 2019</td>
+											<td>October, 2014 - March, 2015</td>
 										</tr>
 									</tbody>
 								</table>
@@ -100,61 +106,72 @@
 						</div>
 
 						<div class="tab-pane fade" id="map">
-							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3642.8910459878716!2d89.04506121464098!3d24.070141084432713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fea10d4f28cc1d%3A0x117abf1575809234!2sRooppur%20Nuclear%20Power%20Plant!5e0!3m2!1sen!2sbd!4v1636509273914!5m2!1sen!2sbd"
+							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9053.906682774377!2d90.32362281178061!3d23.984939412132533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755dd11bfca3641%3A0x47b717eb14aa4a0!2sColor%20City%20Limited%2C%20DBL%20Group!5e0!3m2!1sen!2sbd!4v1636605202456!5m2!1sen!2sbd"
 								allowfullscreen="" loading="lazy"></iframe>
 						</div>
 
 						<div class="tab-pane fade" id="gallery">
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale.jpg"
+							<a href="dist/images/projects/ground-engineering/color-city-project/borehole-drilling-with-hydraulic-rig.jpg"
 								data-sub-html="#c1">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale-thumb.jpg"
-									alt="Cement storage facility at site for uninterrupted supply with weighing scale"/>
+									src="dist/images/projects/ground-engineering/color-city-project/thumb/borehole-drilling-with-hydraulic-rig-450w.jpg"
+									alt="Borehole drilling with hydraulic rig"/>
 								<p class="caption" id="c1"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deep-cement-soil-mixing-with-hyhdraulic-rig-machine.jpg"
+							<a href="dist/images/projects/ground-engineering/color-city-project/casting-via-transit-mixer.jpg"
 								data-sub-html="#c2">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deep-cement-soil-mixing-with-hyhdraulic-rig-machine-thumb.jpg"
-									alt="Deep cement soil mixing with hyhdraulic rig machine"/>
+									src="dist/images/projects/ground-engineering/color-city-project/thumb/casting-via-transit-mixer-450w.jpg"
+									alt="Casting via transit mixer"/>
 								<p class="caption" id="c2"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deployment-of-multiple-hydraulic-rig-machines.jpg"
+							<a href="dist/images/projects/ground-engineering/color-city-project/cleaning-borehole-with-bucket.jpg"
 								data-sub-html="#c3">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deployment-of-multiple-hydraulic-rig-machines-thumb.jpg"
-									alt="Deployment of multiple hydraulic rig machines"/>
+									src="dist/images/projects/ground-engineering/color-city-project/thumb/cleaning-borehole-with-bucket-450w.jpg"
+									alt="Cleaning borehole with bucket"/>
 								<p class="caption" id="c3"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/hydraulic-rotary-rig-and-excavators-in-operation.jpg"
+							<a href="dist/images/projects/ground-engineering/color-city-project/re-bar-fabrication-yard-at-site.jpg"
 								data-sub-html="#c4">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/hydraulic-rotary-rig-and-excavators-in-operation-thumb.jpg"
-									alt="Hydraulic rotary rig and excavators in operation"/>
+									src="dist/images/projects/ground-engineering/color-city-project/thumb/re-bar-fabrication-yard-at-site-450w.jpg"
+									alt="Re-bar fabrication yard at site"/>
 								<p class="caption" id="c4"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/simultaneously-working-hydraulic-rig-machines-at-site.jpg"
+							<a href="dist/images/projects/ground-engineering/color-city-project/re-bar-lowering.jpg"
 								data-sub-html="#c5">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/simultaneously-working-hydraulic-rig-machines-at-site-thumb.jpg"
-									alt="Simultaneously working hydraulic rig machines at site"/>
+									src="dist/images/projects/ground-engineering/color-city-project/thumb/re-bar-lowering-450w.jpg"
+									alt="Re-bar lowering"/>
 								<p class="caption" id="c5"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ground-engineering/color-city-project/re-bar-welding.jpg"
+								data-sub-html="#c6">
+								<img
+									src="dist/images/projects/ground-engineering/color-city-project/thumb/re-bar-welding-450w.jpg"
+									alt="Re-bar welding"/>
+								<p class="caption" id="c6"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
