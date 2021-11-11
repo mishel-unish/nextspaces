436555d7d5db3f6d97591f8a08054892d5f10a7c
Mishel <mishel.unish@gmail.com>
2021-11-11 10:51:00 +0600

    Complete dhaka mass rapid transit project page

--- dhaka-mass-rapid-transit-development.php
+++ dhaka-mass-rapid-transit-development.php
@@ -18,11 +18,13 @@
 			<?php include './shared/header.html'; ?>
 
 			<!-- Head Section -->
-			<section class="small-section rnpp-bg mt-70">
+			<section class="small-section dmrtd-bg mt-70">
 				<div class="relative container align-left">
-					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Rooppur Nuclear Power Plant</h1>
+					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Dhaka Mass Rapid Transit</h1>
 					<div class="mod-breadcrumbs font-alt">
-						<a href="index.php">Home</a> / <a href="ground-engineering.php">Ground Engineering</a> / <span>Rooppur Nuclear Power Plant</span>
+						<a href="index.php">Home</a> /
+						<a href="ground-engineering.php">Ground Engineering</a> /
+						<span>Dhaka Mass Rapid Transit</span>
 					</div>
 				</div>
 			</section>
@@ -64,35 +66,37 @@
 										<tr>
 											<th><i class="fa fa-file-text" aria-hidden="true"></i></th>
 											<th>Project Name</th>
-											<td>Rooppur Nuclear Power Plant Project</td>
+											<td>Dhaka Mass Rapid Transit Project (DMRTDP), Line 06 (CP03-CP04)</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-map-marker" aria-hidden="true"></i></th>
 											<th>Location</th>
-											<td>Rooppur, Ishwardi, Pabna</td>
+											<td>Mirpur, Dhaka, Bangladesh</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-user" aria-hidden="true"></i></th>
 											<th>Contractor</th>
 											<td>
-												<img src="dist/images/contractor/Atomstroyexport-logo.png"
-													alt="Atomstroyexport logo">
-												Atomstroyexport, Joint Stock Company, Russian Federation
+												<img src="dist/images/contractor/italian-thai-development.png"
+													alt="Italian-Thai Development Public Company Limited logo">
+												Italian-Thai Development Public Company Limited
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-files-o" aria-hidden="true"></i></th>
 											<th>Scope of Work</th>
 											<td>
-												Priority construction and installation work of preparatory
-												period at the site of Rooppur Nuclear Power Plant and supply
-												equipment including performance of construction and erection works
+												Subcontract for trial pit cutting for boring and permanent
+												piling, bored cast in situ piles, rebar fabrication works,
+												superstructure works (Pile Cap, Pier/Column & Pier head),
+												portal pier substruction works (pier/column and cross beam)
+												and production of precast viaduct segment (Single and Double Track)
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
 											<th>Duration</th>
-											<td>October, 2016 - June, 2019</td>
+											<td>October, 2017 - October, 2020</td>
 										</tr>
 									</tbody>
 								</table>
@@ -100,61 +104,72 @@
 						</div>
 
 						<div class="tab-pane fade" id="map">
-							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3642.8910459878716!2d89.04506121464098!3d24.070141084432713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fea10d4f28cc1d%3A0x117abf1575809234!2sRooppur%20Nuclear%20Power%20Plant!5e0!3m2!1sen!2sbd!4v1636509273914!5m2!1sen!2sbd"
+							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.0801613972008!2d90.39943051463392!3d23.74452068459227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8922400b1c9%3A0xff3ddb0212e7dfd2!2sDhaka%20Mass%20Rapid%20Transit%20Development%20Projcect!5e0!3m2!1sen!2sbd!4v1636605931017!5m2!1sen!2sbd"
 								allowfullscreen="" loading="lazy"></iframe>
 						</div>
 
 						<div class="tab-pane fade" id="gallery">
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale.jpg"
+							<a href="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/casting-with-boompump.jpg"
 								data-sub-html="#c1">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale-thumb.jpg"
-									alt="Cement storage facility at site for uninterrupted supply with weighing scale"/>
+									src="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/thumb/casting-with-boompump-450w.jpg"
+									alt="Casting with boompump"/>
 								<p class="caption" id="c1"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deep-cement-soil-mixing-with-hyhdraulic-rig-machine.jpg"
+							<a href="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/pier-base-fabrication.jpg"
 								data-sub-html="#c2">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deep-cement-soil-mixing-with-hyhdraulic-rig-machine-thumb.jpg"
-									alt="Deep cement soil mixing with hyhdraulic rig machine"/>
+									src="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/thumb/pier-base-fabrication-450w.jpg"
+									alt="Pier base fabrication"/>
 								<p class="caption" id="c2"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deployment-of-multiple-hydraulic-rig-machines.jpg"
+							<a href="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/pile-cap-and-pier-base-construction.jpg"
 								data-sub-html="#c3">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deployment-of-multiple-hydraulic-rig-machines-thumb.jpg"
-									alt="Deployment of multiple hydraulic rig machines"/>
+									src="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/thumb/pile-cap-and-pier-base-construction-450w.jpg"
+									alt="Pile cap and pier base construction"/>
 								<p class="caption" id="c3"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/hydraulic-rotary-rig-and-excavators-in-operation.jpg"
+							<a href="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/re-bar-fabrication-yard.jpg"
 								data-sub-html="#c4">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/hydraulic-rotary-rig-and-excavators-in-operation-thumb.jpg"
-									alt="Hydraulic rotary rig and excavators in operation"/>
+									src="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/thumb/re-bar-fabrication-yard-450w.jpg"
+									alt="Re-bar fabrication yard"/>
 								<p class="caption" id="c4"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
 								</div>
 							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/simultaneously-working-hydraulic-rig-machines-at-site.jpg"
+							<a href="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/shutterworks-for-station.jpg"
 								data-sub-html="#c5">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/simultaneously-working-hydraulic-rig-machines-at-site-thumb.jpg"
-									alt="Simultaneously working hydraulic rig machines at site"/>
+									src="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/thumb/shutterworks-for-station-450w.jpg"
+									alt="Shutterworks for station"/>
 								<p class="caption" id="c5"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/test-boring-at-site.jpg"
+								data-sub-html="#c6">
+								<img
+									src="dist/images/projects/ground-engineering/dhaka-mass-rapid-transit-development/thumb/test-boring-at-site-450w.jpg"
+									alt="Test boring at site"/>
+								<p class="caption" id="c6"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
