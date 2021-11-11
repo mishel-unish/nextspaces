508a151a2a3d9fa3a8c846d870b116237124d4b1
Mishel <mishel.unish@gmail.com>
2021-11-11 12:21:33 +0600

    Complete brac university project page

--- brac-university.php
+++ brac-university.php
@@ -18,13 +18,13 @@
 			<?php include './shared/header.html'; ?>
 
 			<!-- Head Section -->
-			<section class="small-section rnpp-bg mt-70">
+			<section class="small-section brac-bg mt-70">
 				<div class="relative container align-left">
-					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">RooppurNuclearPowerPlant</h1>
+					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">BRAC University Campus</h1>
 					<div class="mod-breadcrumbs font-alt">
 						<a href="index.php">Home</a> /
 						<a href="ground-engineering.php">Ground Engineering</a> /
-						<span>RooppurNuclearPowerPlant</span>
+						<span>BRAC University Campus</span>
 					</div>
 				</div>
 			</section>
@@ -66,33 +66,35 @@
 										<tr>
 											<th><i class="fa fa-file-text" aria-hidden="true"></i></th>
 											<th>Project Name</th>
-											<td>RooppurNuclear</td>
+											<td>BRAC University New Campus Project</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-map-marker" aria-hidden="true"></i></th>
 											<th>Location</th>
-											<td>RooppurIshwardiPabna</td>
+											<td>Kha – 224, Merul Badda, Dhaka - 1212, Bangladesh</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-user" aria-hidden="true"></i></th>
 											<th>Contractor</th>
 											<td>
-												<img src="dist/images/contractor"
-													alt="Atomstroyexport logo">
-												Atomstroyexport
+												<img src="dist/images/contractor/brac-university.svg"
+													alt="BRAC University logo">
+												BRAC University
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-files-o" aria-hidden="true"></i></th>
 											<th>Scope of Work</th>
 											<td>
-												Priorityconstruction
+												Construction of foundation Pile, Diaphragm Walls and other
+												related works for 13 storied (with 3 basements) institutional
+												building for BRAC University.
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
 											<th>Duration</th>
-											<td>October2016 - June2019</td>
+											<td>March, 2016 - June, 2017</td>
 										</tr>
 									</tbody>
 								</table>
@@ -100,17 +102,72 @@
 						</div>
 
 						<div class="tab-pane fade" id="map">
-							<iframe class="map" src="https"
+							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.290675182787!2d90.42032421463449!3d23.77266133457846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78e49160331%3A0x2fd8ec432cec9fc4!2sBRAC%20University!5e0!3m2!1sen!2sbd!4v1636611600138!5m2!1sen!2sbd"
 								allowfullscreen="" loading="lazy"></iframe>
 						</div>
 
 						<div class="tab-pane fade" id="gallery">
-							<a href="dist/images/projects/ground-engineering"
+							<a href="dist/images/projects/ground-engineering/brac-university/borehole-drilling-with-hydraulic-rig.jpg"
 								data-sub-html="#c1">
 								<img
-									src="dist/images/projects/ground-engineering"
-									alt="Cement"/>
+									src="dist/images/projects/ground-engineering/brac-university/thumb/borehole-drilling-with-hydraulic-rig-450w.jpg"
+									alt="Borehole drilling with hydraulic rig"/>
 								<p class="caption" id="c1"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ground-engineering/brac-university/crawler-cranes-in-operation.jpg"
+								data-sub-html="#c2">
+								<img
+									src="dist/images/projects/ground-engineering/brac-university/thumb/crawler-cranes-in-operation-450w.jpg"
+									alt="Crawler cranes in operation"/>
+								<p class="caption" id="c2"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ground-engineering/brac-university/excavation-with-grab-gk-5.jpg"
+								data-sub-html="#c3">
+								<img
+									src="dist/images/projects/ground-engineering/brac-university/thumb/excavation-with-grab-gk-5-450w.jpg"
+									alt="Excavation with Grab GK-5"/>
+								<p class="caption" id="c3"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ground-engineering/brac-university/excavation-with-grab-gk-5-for-d-wall.jpg"
+								data-sub-html="#c4">
+								<img
+									src="dist/images/projects/ground-engineering/brac-university/thumb/excavation-with-grab-gk-5-for-d-wall-450w.jpg"
+									alt="Excavation with Grab GK-5 for D-wall"/>
+								<p class="caption" id="c4"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ground-engineering/brac-university/re-bar-for-d-wall.jpg"
+								data-sub-html="#c5">
+								<img
+									src="dist/images/projects/ground-engineering/brac-university/thumb/re-bar-for-d-wall-450w.jpg"
+									alt="Re-bar for D-wall"/>
+								<p class="caption" id="c5"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/ground-engineering/brac-university/re-bar-lowering-and-welding.jpg"
+								data-sub-html="#c6">
+								<img
+									src="dist/images/projects/ground-engineering/brac-university/thumb/re-bar-lowering-and-welding-450w.jpg"
+									alt="Re-bar lowering and welding"/>
+								<p class="caption" id="c6"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
