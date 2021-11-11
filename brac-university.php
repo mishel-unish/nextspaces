dcd8728d8436ce70b064a0e440a651242e88a396
Mishel <mishel.unish@gmail.com>
2021-11-11 10:02:49 +0600

    Create all project bg class css

--- brac-university.php
+++ brac-university.php
@@ -20,9 +20,11 @@
 			<!-- Head Section -->
 			<section class="small-section rnpp-bg mt-70">
 				<div class="relative container align-left">
-					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Rooppur Nuclear Power Plant</h1>
+					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">RooppurNuclearPowerPlant</h1>
 					<div class="mod-breadcrumbs font-alt">
-						<a href="index.php">Home</a> / <a href="ground-engineering.php">Ground Engineering</a> / <span>Rooppur Nuclear Power Plant</span>
+						<a href="index.php">Home</a> /
+						<a href="ground-engineering.php">Ground Engineering</a> /
+						<span>RooppurNuclearPowerPlant</span>
 					</div>
 				</div>
 			</section>
@@ -64,35 +66,33 @@
 										<tr>
 											<th><i class="fa fa-file-text" aria-hidden="true"></i></th>
 											<th>Project Name</th>
-											<td>Rooppur Nuclear Power Plant Project</td>
+											<td>RooppurNuclear</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-map-marker" aria-hidden="true"></i></th>
 											<th>Location</th>
-											<td>Rooppur, Ishwardi, Pabna</td>
+											<td>RooppurIshwardiPabna</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-user" aria-hidden="true"></i></th>
 											<th>Contractor</th>
 											<td>
-												<img src="dist/images/contractor/Atomstroyexport-logo.png"
+												<img src="dist/images/contractor"
 													alt="Atomstroyexport logo">
-												Atomstroyexport, Joint Stock Company, Russian Federation
+												Atomstroyexport
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-files-o" aria-hidden="true"></i></th>
 											<th>Scope of Work</th>
 											<td>
-												Priority construction and installation work of preparatory
-												period at the site of Rooppur Nuclear Power Plant and supply
-												equipment including performance of construction and erection works
+												Priorityconstruction
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
 											<th>Duration</th>
-											<td>October, 2016 - June, 2019</td>
+											<td>October2016 - June2019</td>
 										</tr>
 									</tbody>
 								</table>
@@ -100,61 +100,17 @@
 						</div>
 
 						<div class="tab-pane fade" id="map">
-							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3642.8910459878716!2d89.04506121464098!3d24.070141084432713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fea10d4f28cc1d%3A0x117abf1575809234!2sRooppur%20Nuclear%20Power%20Plant!5e0!3m2!1sen!2sbd!4v1636509273914!5m2!1sen!2sbd"
+							<iframe class="map" src="https"
 								allowfullscreen="" loading="lazy"></iframe>
 						</div>
 
 						<div class="tab-pane fade" id="gallery">
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale.jpg"
+							<a href="dist/images/projects/ground-engineering"
 								data-sub-html="#c1">
 								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/cement-storage-facility-at-site-for-uninterrupted-supply-with-weighing-scale-thumb.jpg"
-									alt="Cement storage facility at site for uninterrupted supply with weighing scale"/>
+									src="dist/images/projects/ground-engineering"
+									alt="Cement"/>
 								<p class="caption" id="c1"></p>
-								<div class="view">
-									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
-									View Gallery
-								</div>
-							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deep-cement-soil-mixing-with-hyhdraulic-rig-machine.jpg"
-								data-sub-html="#c2">
-								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deep-cement-soil-mixing-with-hyhdraulic-rig-machine-thumb.jpg"
-									alt="Deep cement soil mixing with hyhdraulic rig machine"/>
-								<p class="caption" id="c2"></p>
-								<div class="view">
-									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
-									View Gallery
-								</div>
-							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/deployment-of-multiple-hydraulic-rig-machines.jpg"
-								data-sub-html="#c3">
-								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/deployment-of-multiple-hydraulic-rig-machines-thumb.jpg"
-									alt="Deployment of multiple hydraulic rig machines"/>
-								<p class="caption" id="c3"></p>
-								<div class="view">
-									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
-									View Gallery
-								</div>
-							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/hydraulic-rotary-rig-and-excavators-in-operation.jpg"
-								data-sub-html="#c4">
-								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/hydraulic-rotary-rig-and-excavators-in-operation-thumb.jpg"
-									alt="Hydraulic rotary rig and excavators in operation"/>
-								<p class="caption" id="c4"></p>
-								<div class="view">
-									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
-									View Gallery
-								</div>
-							</a>
-							<a href="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/simultaneously-working-hydraulic-rig-machines-at-site.jpg"
-								data-sub-html="#c5">
-								<img
-									src="dist/images/projects/ground-engineering/rooppur-nuclear-power-plant/thumb/simultaneously-working-hydraulic-rig-machines-at-site-thumb.jpg"
-									alt="Simultaneously working hydraulic rig machines at site"/>
-								<p class="caption" id="c5"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
