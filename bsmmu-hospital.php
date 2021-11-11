fe1712c79957cf52b88642817eead4a4e81e74d7
Mishel <mishel.unish@gmail.com>
2021-11-11 15:57:20 +0600

    Complete bsmmu hospital project page

--- bsmmu-hospital.php
+++ bsmmu-hospital.php
@@ -18,13 +18,13 @@
 			<?php include './shared/header.html'; ?>
 
 			<!-- Head Section -->
-			<section class="small-section rnpp-bg mt-70">
+			<section class="small-section bsmmu-bg mt-70">
 				<div class="relative container align-left">
-					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">RooppurNuclearPowerPlant</h1>
+					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">BSMMU Hospital</h1>
 					<div class="mod-breadcrumbs font-alt">
 						<a href="index.php">Home</a> /
 						<a href="construction.php">Construction</a> /
-						<span>RooppurNuclearPowerPlant</span>
+						<span>BSMMU Hospital</span>
 					</div>
 				</div>
 			</section>
@@ -66,33 +66,34 @@
 										<tr>
 											<th><i class="fa fa-file-text" aria-hidden="true"></i></th>
 											<th>Project Name</th>
-											<td>RooppurNuclear</td>
+											<td>BSMMU Super Specialized Hospital Construction Project</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-map-marker" aria-hidden="true"></i></th>
 											<th>Location</th>
-											<td>RooppurIshwardiPabna</td>
+											<td>Shahbag, Dhaka, Bangladesh</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-user" aria-hidden="true"></i></th>
 											<th>Contractor</th>
 											<td>
-												<img src="dist/images/contractor"
-													alt="Atomstroyexport logo">
-												Atomstroyexport
+												<img src="dist/images/contractor/hyundai-development-company.png"
+													alt="Hyundai Development Company (HDC) logo">
+												Hyundai Development Company (HDC)
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-files-o" aria-hidden="true"></i></th>
 											<th>Scope of Work</th>
 											<td>
-												Priorityconstruction
+												Construction for establishment of a super-specialized hospital
+												under Bangabandhu Sheikh Mujib Medical University (BSMMU)
 											</td>
 										</tr>
 										<tr>
 											<th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
 											<th>Duration</th>
-											<td>October2016 - June2019</td>
+											<td>August, 2018 - June, 2020</td>
 										</tr>
 									</tbody>
 								</table>
@@ -100,17 +101,72 @@
 						</div>
 
 						<div class="tab-pane fade" id="map">
-							<iframe class="map" src="https"
+							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.2118976477473!2d90.39248801463376!3d23.73982198459455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8954649cee5%3A0x3bdcd530be93b17a!2sBangabandhu%20Sheikh%20Mujib%20Medical%20University%20Hospital!5e0!3m2!1sen!2sbd!4v1636624260627!5m2!1sen!2sbd"
 								allowfullscreen="" loading="lazy"></iframe>
 						</div>
 
 						<div class="tab-pane fade" id="gallery">
-							<a href="dist/images/projects/construction"
-								data-sub-html="#c">
+							<a href="dist/images/projects/construction/bsmmu-super-specialized-hospital/construction-of-multistoried-hospital-building.jpg"
+								data-sub-html="#c1">
 								<img
-									src="dist/images/projects/construction"
-									alt="Cement"/>
-								<p class="caption" id="c"></p>
+									src="dist/images/projects/construction/bsmmu-super-specialized-hospital/thumb/construction-of-multistoried-hospital-building-450w.jpg"
+									alt="Construction of multistoried hospital building"/>
+								<p class="caption" id="c1"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/construction/bsmmu-super-specialized-hospital/operational-tower-crane-at-site.jpg"
+								data-sub-html="#c2">
+								<img
+									src="dist/images/projects/construction/bsmmu-super-specialized-hospital/thumb/operational-tower-crane-at-site-450w.jpg"
+									alt="Operational tower crane at site"/>
+								<p class="caption" id="c2"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/construction/bsmmu-super-specialized-hospital/scaffolding-for-multistoried-hospital-building-1.jpg"
+								data-sub-html="#c3">
+								<img
+									src="dist/images/projects/construction/bsmmu-super-specialized-hospital/thumb/scaffolding-for-multistoried-hospital-building-1-450w.jpg"
+									alt="Scaffolding for multistoried hospital building"/>
+								<p class="caption" id="c3"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/construction/bsmmu-super-specialized-hospital/scaffolding-for-multistoried-hospital-building-2.jpg"
+								data-sub-html="#c4">
+								<img
+									src="dist/images/projects/construction/bsmmu-super-specialized-hospital/thumb/scaffolding-for-multistoried-hospital-building-2-450w.jpg"
+									alt="Scaffolding for multistoried hospital building"/>
+								<p class="caption" id="c4"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/construction/bsmmu-super-specialized-hospital/scaffolding-for-multistoried-hospital-building-3.jpg"
+								data-sub-html="#c5">
+								<img
+									src="dist/images/projects/construction/bsmmu-super-specialized-hospital/thumb/scaffolding-for-multistoried-hospital-building-3-450w.jpg"
+									alt="Scaffolding for multistoried hospital building"/>
+								<p class="caption" id="c5"></p>
+								<div class="view">
+									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+									View Gallery
+								</div>
+							</a>
+							<a href="dist/images/projects/construction/bsmmu-super-specialized-hospital/shore-pile-for-basement-and-underground-parking.jpg"
+								data-sub-html="#c6">
+								<img
+									src="dist/images/projects/construction/bsmmu-super-specialized-hospital/thumb/shore-pile-for-basement-and-underground-parking-450w.jpg"
+									alt="Shore pile for basement and underground parking"/>
+								<p class="caption" id="c6"></p>
 								<div class="view">
 									<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
 									View Gallery
