2c2065dbe97a7ea19d56e5da2d857f3f2eae9431
Mishel <mishel.unish@gmail.com>
2021-11-21 20:12:16 +0600

    Complete capabilities page

--- capabilities.php
+++ capabilities.php
@@ -4,7 +4,7 @@
 	<!-- Head -->
 	<?php include './shared/head.html'; ?>
 
-	<body class="appear-animate child-page">
+	<body id="capabilities_page" class="appear-animate child-page">
 		<!-- Page Loader -->
 		<div class="page-loader">
 			<div class="loader">Loading...</div>
@@ -18,7 +18,7 @@
 			<?php include './shared/header.html'; ?>
 
 			<!-- Head Section -->
-			<header class="small-section topbar-bg mt-70">
+			<header class="small-section header-container bg-gray-lighter mt-70">
 				<div class="relative container align-left">
 					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Capabilities</h1>
 					<div class="mod-breadcrumbs font-alt">
@@ -26,6 +26,301 @@
 					</div>
 				</div>
 			</header>
+
+			<main>
+				<div class="container">
+					<div class="row">
+
+						<div class="col-md-8 col-md-offset-2">
+							<p class="para-middle">
+								Next Spaces Limited has always been dedicated to developing
+								and managing the planning and construction of infrastructure
+								& building projects with the introduction of innovative modern
+								technology. Professionals who have already scored their mark
+								of excellence in the field of engineering & construction
+								are always dedicated to ensure quality and on time delivery
+								according to our commitment. Enthusiastic supervising with a
+								vision to look forward to take any challenge makes us unique
+								and capable around the country for any construction or ground
+								engineering project.
+							</p>
+						</div>
+						<div class="clearfix"></div>
+
+						<div class="col-md-12">
+							<h2 class="section-title font-alt custom-title-1">List of equipments</h2>
+							<div id="equipments" class="equipments">
+
+								<a class="equipment" href="dist/images/capabilities/1920w/abi-tm-22-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/abi-tm-22-400w.jpg" alt="ABI TM-22">
+									<div class="info">
+										<h3 class="model">ABI TM-22</h3>
+										<p class="manufacturer">ABI Equipments Ltd.</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/bauer-bg-20h-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/bauer-bg-20h-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">BAUER BG 20H</h3>
+										<p class="manufacturer">BAUER</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/bauer-bg-28-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/bauer-bg-28-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">BAUER BG-28</h3>
+										<p class="manufacturer">BAUER</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/misc/bauer-gb-40-1280w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/bauer-gb-40-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">BAUER GB 40</h3>
+										<p class="manufacturer">BAUER</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/caterpillar-312d-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/caterpillar-312d-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Caterpillar 312D</h3>
+										<p class="manufacturer">Caterpillar, USA</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/caterpillar-314ccr-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/caterpillar-314ccr-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Caterpillar 314CCR</h3>
+										<p class="manufacturer">Caterpillar, USA</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/caterpillar-320d-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/caterpillar-320d-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Caterpillar 320D</h3>
+										<p class="manufacturer">Caterpillar, USA</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/misc/fiori-dbx-50-1024w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/fiori-dbx-50-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">FIORI DBX-50</h3>
+										<p class="manufacturer">FIORI</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/fuwa-fwx-55-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/fuwa-fwx-55-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">FUWA FWX-55</h3>
+										<p class="manufacturer">FUWA MACHINERY INTERNATIONAL</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/giken-sa-100-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/giken-sa-100-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">GIKEN SA100</h3>
+										<p class="manufacturer">GIKEN</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/kyb-conmat-crp-450-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/kyb-conmat-crp-450-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">KYB-CONMAT CRP 450</h3>
+										<p class="manufacturer">KYB-CONMAT</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/potain-mci-185-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/potain-mci-185-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Potain MCI 185</h3>
+										<p class="manufacturer">Potain, Germany</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/sany-sy210c-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/sany-sy210c-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">SANY SY210C</h3>
+										<p class="manufacturer">SANY, India</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/sany-sy140c-9s-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/sany-sy140c-9s-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Sany SY140C-9S</h3>
+										<p class="manufacturer">SANY, India</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/schwing-stetter-am6shn2-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/schwing-stetter-am6shn2-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Schwing Stetter AM6SHN2</h3>
+										<p class="manufacturer">Schwing Stetter</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/schwing-stetter-m1-twin-shaft-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/schwing-stetter-m1-twin-shaft-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Schwing Stetter M1 Twin Shaft</h3>
+										<p class="manufacturer">Schwing Stetter</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/misc/simem-jumper-75hd-500w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/simem-jumper-75hd-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Simem Jumper 75HD</h3>
+										<p class="manufacturer">Simem Jumper</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/soilmec-grab-gk-5-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/soilmec-grab-gk-5-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">SOILMEC Grab GK-5</h3>
+										<p class="manufacturer">SOILMEC, ITALY</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/soilmec-sr-30-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/soilmec-sr-30-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">SOILMEC SR-30</h3>
+										<p class="manufacturer">SOILMEC, ITALY</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/soilmec-sr-40-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/soilmec-sr-40-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">SOILMEC SR-40</h3>
+										<p class="manufacturer">SOILMEC, ITALY</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/misc/soilmec-sr-60-1280w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/soilmec-sr-60-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">SOILMEC SR-60</h3>
+										<p class="manufacturer">SOILMEC, ITALY</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+								<a class="equipment" href="dist/images/capabilities/1920w/zoomlion-rt35-1920w.jpg" data-sub-html=".info">
+									<img src="dist/images/capabilities/400w/zoomlion-rt35-400w.jpg" alt="">
+									<div class="info">
+										<h3 class="model">Zoomlion RT35</h3>
+										<p class="manufacturer">Zoomlion, China</p>
+									</div>
+									<div class="visit">
+										<i class="fa fa-hand-pointer-o" aria-hidden="true"></i>
+										Visit Gallery
+									</div>
+								</a>
+
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</main>
 
 
 
@@ -42,5 +337,13 @@
 			}());
 		</script>
 		<?php include './shared/scripts.html'; ?>
+		<script>
+			lightGallery(document.getElementById("equipments"), {
+				thumbnail:true,
+				download: false,
+				caption: true,
+				subHtmlSelectorRelative: true
+			});
+		</script>
 	</body>
 </html>
