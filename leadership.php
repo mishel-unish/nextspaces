beca23772195037ee0f8bd10e84f449445750ad0
Mishel <mishel.unish@gmail.com>
2021-12-19 13:12:11 +0600

    Complete Leadership page

--- leadership.php
+++ leadership.php
@@ -29,61 +29,111 @@
 
 			<main class="page-sectionx">
 				<div class="container">
-					<div class="leader-comp para-left">
+					<div class="leader-comp mb-50 mb-xs-40">
 						<figure class="leader-fig">
-							<img class="img-responsive" src="dist/images/team/bimal-cr.jpg" alt="">
-							<figcaption class="text-center">
-								<strong>Bimal Chandra Roy</strong>
-								<br>
-								Managing Director
+							<img
+								src="dist/images/team/bimal-cr.jpg"
+								alt="Image of Managing Director Bimal Chandra Roy">
+							<figcaption>
+								<span class="name">Bimal Chandra Roy</span>
+								<span class="position">Managing Director</span>
 							</figcaption>
 						</figure>
-						<div class="leader-text">
-							<p class="lead text-bold">
-								Civil Engineer, 35 Years of Experience in Construction having working experience both in Local and MNC companies Worldwide.
-							</p>
-							<p>
-								Project Engineer with <strong>DAEWOO CORPORATION</strong> of South Korea at Central Locomotive Workshop project of Bangladesh Railway at Parbatipur. Experiences gathered on Construction of pile foundation, pile load test, construction of workshop building, fabrication and erection of steel structure, overhead water tower, machine foundations. <strong>Project Engineer with BIT spa</strong> of ITALY and Engineers & Engineers of Bangladesh at two Microwave Tower project at Nandankanan and Agrabad at Chittagong. Experiences on Construction of pile foundation, pile load test, fabrication and erection of steel tower. <strong>Project Manager with Islam Trading Consortium Ltd.</strong> at Coastal Embankment Project at Banchkhali, Chittagong. Construction of 26Km earth embankment at Banchkhali. <strong>Project Manager with Borak Real Estate pvt. Ltd.</strong> in construction of apartment buildings. <strong>Bridge Engineer with SAMWHAN CORPORATION</strong> of South Korea at LAOS, PDR in construction of three bridges in the river of NAM-OU, NAMXIAN and NAMKHAN. <strong>Structural Engineer with DAEWOO CORPORATION</strong> of South Korea at Kuala Lumpur, Malaysia in the project of PLAZA RAKYAT. Supervision of construction work of 97 storied office tower building, 45 storied service apartment building, 26 storied hotel building and vast retail area. Construction supervision of 6 basement floors. Construction supervision of 3500 Nos cast-in-situ bored pile, construction supervision of pilled mat foundation. <strong>Project Manager with Unique Group Pvt Ltd.</strong> in construction of 20 storied UTC building with 3 basement floors at Panthapath, 29 storied hotel building (THE WESTIN) with 3 basement floors at gulshan-2, 20 storied Red Crescent-Borak Tower building. <strong>Project Manager, Incepta Pharmaceuticals Limited</strong> from 2002 to 2010, where approximately 10,00,000 sft of Industrial Production Facility was constructed during this service. <strong>Head of the Department</strong>, Engineering & Construction of <strong>Bay Developments Ltd.</strong> from 2010 to 2011.
-							</p>
-						</div>
+						<p class="lead">
+							Civil Engineer, 35 Years of Experience in Construction having
+							working experience both in Local and MNC companies Worldwide.
+						</p>
+						<p class="main-text">
+							Project Engineer with DAEWOO CORPORATION of South Korea at
+							Central Locomotive Workshop project of Bangladesh Railway at
+							Parbatipur. Experiences gathered on Construction of pile
+							foundation, pile load test, construction of workshop building,
+							fabrication and erection of steel structure, overhead water
+							tower, machine foundations. Project Engineer with BIT spa of
+							ITALY and Engineers & Engineers of Bangladesh at two Microwave
+							Tower project at Nandankanan and Agrabad at Chittagong.
+							Experiences on Construction of pile foundation, pile load test,
+							fabrication and erection of steel tower. Project Manager with
+							Islam Trading Consortium Ltd. at Coastal Embankment Project at
+							Banchkhali, Chittagong. Construction of 26Km earth embankment at
+							Banchkhali. Project Manager with Borak Real Estate pvt. Ltd. in
+							construction of apartment buildings. Bridge Engineer with SAMWHAN
+							CORPORATION of South Korea at LAOS, PDR in construction of three
+							bridges in the river of NAM-OU, NAMXIAN and NAMKHAN. Structural
+							Engineer with DAEWOO CORPORATION of South Korea at Kuala Lumpur,
+							Malaysia in the project of PLAZA RAKYAT. Supervision of construction
+							work of 97 storied office tower building, 45 storied service
+							apartment building, 26 storied hotel building and vast retail
+							area. Construction supervision of 6 basement floors. Construction
+							supervision of 3500 Nos cast-in-situ bored pile, construction
+							supervision of pilled mat foundation. Project Manager with Unique
+							Group Pvt Ltd. in construction of 20 storied UTC building with 3
+							basement floors at Panthapath, 29 storied hotel building (THE WESTIN)
+							with 3 basement floors at gulshan-2, 20 storied Red Crescent-Borak
+							Tower building. Project Manager, Incepta Pharmaceuticals Limited
+							from 2002 to 2010, where approximately 10,00,000 sft of Industrial
+							Production Facility was constructed during this service. Head of
+							the Department, Engineering & Construction of Bay Developments
+							Ltd. from 2010 to 2011.
+						</p>
 					</div>
 
-					<div class="leader-comp para-right" style="margin-top: 70px;">
-						<div class="leader-text2">
-							<p class="lead text-bold">
-								Architect, more than two decades of experience in Design & Construction Industry.
-							</p>
-							<p>
-								Graduated from Bangladesh University of Engineering & Technology <strong>[BUET]</strong> Dhaka in 1997. He received the <strong>Commonwealth Association of Architects [CAA] Award</strong> in <strong>1994</strong> for the design of a residential building at Dhaka. Mr. Hassan is also an <strong>Architect-Partner</strong> of <strong>Neoformation Architects</strong>, where he is the team-leader in <strong>Pharmaceutical Projects</strong> of Neoformation Architects, specializing in the designing of clean room facility for pharma production. One of his current project is the master plan of a 100 acres’ of industrial plant for <strong>Incepta Pharmaceutical Ltd.</strong> at Dhamrai, Savar, Bangladesh, which currently consists of 6 Nos multi-storied production building, (10,00,000 sft) for multi-disciplinary pharma production facilities mostly engaged in producing drugs for international markets.
-							</p>
-						</div>
-						<figure class="leader-fig2">
-							<img class="img-responsive" src="dist/images/team/khondkar-nh.jpg" alt="">
-							<figcaption class="text-center">
-								<strong>Khondkar Nazmul Hassan</strong>
-								<br>
-								Architect - Director
+					<div class="leader-comp mb-50 mb-xs-40">
+						<figure class="leader-fig">
+							<img
+								src="dist/images/team/khondkar-nh.jpg"
+								alt="Image of Director Architect Khondkar Nazmul Hassan">
+							<figcaption>
+								<span class="name">Khondkar Nazmul Hassan</span>
+								<span class="position">Architect - Director</span>
 							</figcaption>
 						</figure>
+						<p class="lead">
+							Architect, more than two decades of experience in Design & Construction Industry.
+						</p>
+						<p class="main-text">
+							Graduated from Bangladesh University of Engineering & Technology
+							[BUET] Dhaka in 1997. He received the Commonwealth Association of
+							Architects [CAA] Award in 1994 for the design of a residential
+							building at Dhaka. Mr. Hassan is also an Architect-Partner of
+							Neoformation Architects, where he is the team-leader in Pharmaceutical
+							Projects of Neoformation Architects, specializing in the designing
+							of clean room facility for pharma production. One of his current
+							project is the master plan of a 100 acres’ of industrial plant
+							for Incepta Pharmaceutical Ltd. at Dhamrai, Savar, Bangladesh,
+							which currently consists of 6 Nos multi-storied production building,
+							(10,00,000 sft) for multi-disciplinary pharma production facilities
+							mostly engaged in producing drugs for international markets.
+						</p>
 					</div>
 
-					<div class="leader-comp para-left" style="margin-top: 70px;">
+					<div class="leader-comp">
 						<figure class="leader-fig">
-							<img class="img-responsive" src="dist/images/team/khalid-ak.jpg" alt="">
-							<figcaption class="text-center">
-								<strong>Khalid Ahmed Khan</strong>
-								<br>
-								Architect - Director
+							<img
+								src="dist/images/team/khalid-ak.jpg"
+								alt="Image of Director Architect Khalid Ahmed Khan">
+							<figcaption>
+								<span class="name">Khalid Ahmed Khan</span>
+								<span class="position">Architect - Director</span>
 							</figcaption>
 						</figure>
-						<div class="leader-text">
-							<p class="lead text-bold">
-								Architect, more than two decades of experience in Design & Construction Industry.
-							</p>
-							<p>
-								Graduated from the <strong>Bangladesh University of Engineering & Technology [BUET]</strong>, Dhaka in 1997. An Architect-partner of “Neoformation Architects”, Architect-Director of KNMR LTD. and Director of Next Spaces ltd.  Mr. Khan is also a visiting faculty at the Dept. of Architecture, Ahsanullah University of Science & Technology, Dhaka and BRAC University, Dhaka. He acts as the team-leader in Neoformation Architect’s industrial projects that include textile, knit and woven plants in and around Dhaka. He is currently engaged with the development of a Hi-tech Multipurpose Building of <strong>Incepta Pharmaceuticals Ltd.</strong> (750,000 sft). Majority portion of The Soil stabilization works of Roopur Neuclear Power Plant was mainly executed by Next Spaces Ltd. where Mr. Khan is the project Director since October 2017.
-							</p>
-						</div>
+						<p class="lead">
+							Architect, more than two decades of experience in Design & Construction Industry.
+						</p>
+						<p class="main-text">
+							Graduated from the Bangladesh University of Engineering & Technology
+							[BUET], Dhaka in 1997. An Architect-partner of “Neoformation Architects”,
+							Architect-Director of KNMR LTD. and Director of Next Spaces ltd.  Mr.
+							Khan is also a visiting faculty at the Dept. of Architecture, Ahsanullah
+							University of Science & Technology, Dhaka and BRAC University, Dhaka.
+							He acts as the team-leader in Neoformation Architect’s industrial projects
+							that include textile, knit and woven plants in and around Dhaka. He is
+							currently engaged with the development of a Hi-tech Multipurpose Building
+							of Incepta Pharmaceuticals Ltd. (750,000 sft). Majority portion of The
+							Soil stabilization works of Roopur Neuclear Power Plant was mainly
+							executed by Next Spaces Ltd. where Mr. Khan is the project Director
+							since October 2017.
+						</p>
 					</div>
 				</div>
 			</main>
