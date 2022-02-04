cec2afe7d4a2ea75e37f868a8d784598030c044f
Mishel <mishel.unish@gmail.com>
2022-02-05 02:14:53 +0600

    Complete career page

--- career.php
+++ career.php
@@ -1,23 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	<!-- Head -->
 	<?php include './shared/head.html'; ?>
 
-	<body class="appear-animate child-page">
-		<!-- Page Loader -->
-		<div class="page-loader">
+	<body id="career_page" class="appear-animate child-page">
+		<!-- <div class="page-loader">
 			<div class="loader">Loading...</div>
-		</div>
-		<!-- End Page Loader -->
+		</div> -->
 
 		<!-- Page Wrap -->
 		<div class="page" id="top">
 
-			<!-- Head -->
 			<?php include './shared/header.html'; ?>
 
-			<!-- Head Section -->
 			<header class="small-section header-container bg-gray-lighter mt-70">
 				<div class="relative container align-left">
 					<h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Career</h1>
@@ -28,15 +23,106 @@
 			</header>
 
 			<main>
-				<div class="container"></div>
+				<section class="mb-50">
+					<div class="container">
+						<h2 class="font-alt mt-0">Who we are</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero animi at, odit officia sed cum dolorem laborum nihil accusantium corporis expedita atque numquam nobis eligendi nisi in doloribus eum debitis.
+						</p>
+						<p>
+							<a class="link" href="overview.php">More about us &rightarrow;</a>
+						</p>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2">
+								<div>
+									<figure>
+										<img src="./dist/images/career/off1.jpg" alt="" class="img-responsive">
+										<figcaption>NSL office</figcaption>
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="mb-50">
+					<div class="container">
+						<h2 class="font-alt mt-0">Life at NSL</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam veritatis adipisci iure dolores et possimus quis ullam pariatur vel debitis. Velit eos minus impedit commodi amet voluptatum voluptates magnam id.
+						</p>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur nesciunt sed hic asperiores doloremque harum numquam quidem reprehenderit quas ut sint labore repellendus, facilis assumenda dolore! Voluptas modi obcaecati delectus!
+						</p>
+						<div class="nsl-life">
+							<img src="./dist/images/career/c1.jpg" alt="">
+							<img src="./dist/images/career/c2.jpg" alt="">
+							<img src="./dist/images/career/c3.jpg" alt="">
+							<img src="./dist/images/career/c4.jpg" alt="">
+						</div>
+					</div>
+				</section>
+
+				<section class="mb-50">
+					<div class="container">
+						<h2 class="font-alt mt-0">What is the NSL team like?</h2>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga tenetur porro laudantium veritatis repellat, nisi explicabo quia quam consequatur aut pariatur impedit perspiciatis consequuntur omnis sunt odio optio. Repellat, numquam.
+						</p>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2">
+								<div>
+									<figure>
+										<img src="./dist/images/career/c5.jpg" alt="" class="img-responsive">
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="mb-50">
+					<div class="container">
+						<h2 class="font-alt mt-0">Our values</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolorem, quia tenetur illo ullam voluptates ipsam tempore ea, nesciunt aut ipsum aperiam. Consequuntur cupiditate consequatur, laudantium maxime assumenda distinctio nostrum!
+						</p>
+						<ul class="mb-0">
+							<li>Lorem ipsum dolor sit</li>
+							<li>amet consectetur adipisicing</li>
+							<li>elit. Nobis, ipsa nesciunt</li>
+						</ul>
+					</div>
+				</section>
+
+				<section class="mb-50">
+					<div class="container">
+						<h2 class="font-alt mt-0">Perks &amp; Benefits</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente id repellendus eum assumenda. Porro mollitia velit in possimus officia minima placeat tempora, odit perferendis asperiores temporibus. Iusto, dolorum debitis! Debitis.</p>
+						<ul class="mb-0 perks-ul">
+							<li>
+								<i class="fa fa-usd" aria-hidden="true"></i>
+								Competitive salary
+							</li>
+							<li>
+								<i class="fa fa-medkit" aria-hidden="true"></i>
+								Health insurance
+							</li>
+							<li>
+								<i class="fa fa-plane" aria-hidden="true"></i>
+								Vacation
+							</li>
+						</ul>
+					</div>
+				</section>
+
+				<?php include './components/career/section-job-opening.html'; ?>
 			</main>
 
-			<!-- Footer -->
 			<?php include './shared/footer.html'; ?>
 		</div>
 		<!-- End Page Wrap -->
 
-		<!-- JS -->
 		<script>
 			(function() {
 				document.querySelector("#career a").classList.add("active");
