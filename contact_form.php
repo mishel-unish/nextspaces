251caea7534d49de92fc4c996d63bb068b30c0af
Mishel <mishel.unish@gmail.com>
2022-08-17 02:58:00 +0600

    Add backend code for contact form

---
+++ contact_form.php
@@ -0,0 +1,97 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$msg = "Test Success";
+$type = "form-success";
+$data = array($msg, $type);
+
+$email = new PHPMailer(TRUE);
+
+if($_POST) {
+	$client_name = "";
+	$client_email = "";
+	$client_phone = "";
+	$email_title = "Message from client";
+	// $recipient = "mail@nextspaces.net";
+	$recipient = "mishel.unish@gmail.com";
+	$client_msg = "";
+	$email_body = "<div>";
+	$sender = "web.msg@nextspaces.net";
+	$senderName = 'Nextspaces Web';
+	$host = 'mail.nextspaces.net';
+	$password = '32P@sSke3Yu556Wd102';
+	$port = 465;
+	$security = 'ssl';
+
+	if(isset($_POST['client_name'])) {
+		$client_name = filter_var($_POST['client_name'], FILTER_SANITIZE_STRING);
+		$email_body .= "<div>
+								<label><b>Client Name:</b></label>&nbsp;<span>".$client_name."</span>
+							</div>";
+	}
+
+	if(isset($_POST['client_email'])) {
+		$client_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['client_email']);
+		$client_email = filter_var($client_email, FILTER_VALIDATE_EMAIL);
+		$email_body .= "<div>
+								<label><b>Client's Email:</b></label>&nbsp;<span>".$client_email."</span>
+							</div>";
+	}
+
+	if(isset($_POST['client_phone'])) {
+		$client_phone = filter_var($_POST['client_phone'], FILTER_SANITIZE_STRING);
+		$email_body .= "<div>
+								<label><b>Client's Phone:</b></label>&nbsp;<span>".$client_phone."</span>
+							</div>";
+	}
+
+	if(isset($_POST['client_msg'])) {
+		$client_msg = htmlspecialchars($_POST['client_msg']);
+		$email_body .= "<div>
+								<label><b>Client's Message:</b></label>
+								<div>".$client_msg."</div>
+							</div>";
+	}
+
+	$email_body .= "</div>";
+
+	try {
+
+		$email->setFrom($sender, $senderName);
+		$email->addAddress($recipient);
+		$email->Subject = $email_title;
+		$email->Body = $email_body;
+
+		$email->isSMTP();
+		$email->isHTML(true);
+		$email->Host = $host;
+		$email->SMTPAuth = TRUE;
+		$email->Username = $sender;
+		$email->Password = $password;
+		$email->SMTPSecure = $security;
+		$email->Port = $port;
+		$email->send();
+		$msg = "Thank you for contacting us mr./ms. " .$client_name. ". You will hear from us soon.";
+		$type = "form-success";
+	}
+	catch (Exception $e)
+	{
+		$msg = "Something went wrong. We are sorry for the inconvenience. Please try again.";
+		$type = "form-error";
+	}
+
+} 	else {
+	$msg = "Something went wrong. We are sorry for the inconvenience. Please try again.";
+	$type = "form-error";
+}
+
+$data = array($msg, $type);
+// $data = array($msg, $type, $client_name, $client_email, $client_phone, $client_msg);
+
+echo json_encode($data);
+?>
